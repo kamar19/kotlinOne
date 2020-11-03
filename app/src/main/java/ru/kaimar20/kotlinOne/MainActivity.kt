f435b3872b287db6c99d2605605db0e81886d2ad
kamar19 <kipisov@mail.ru>
2020-11-03 10:55:08 +0600

    Initial commit

---
+++ app/src/main/java/ru/kaimar20/kotlinOne/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.kaimar20.kotlinOne
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
