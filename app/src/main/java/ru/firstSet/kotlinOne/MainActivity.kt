cac132b56232c3de051111937eb7cd2d22957696
kamar19 <kipisov@mail.ru>
2020-11-22 18:50:25 +0600

    Fixed the comments.

--- app/src/main/java/ru/firstSet/kotlinOne/MainActivity.kt
+++ app/src/main/java/ru/firstSet/kotlinOne/MainActivity.kt
@@ -9,27 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val textView: TextView = findViewById(R.id.activityMainTextView1)
+        val textView: TextView = findViewById(R.id.activityMainCenterTextView)
         textView.setOnClickListener{ moveToNextScreen()}
     }
 
     private fun moveToNextScreen() {
         val intent=Intent(this,MovieDetailsActivity::class.java)
-
-        val transmittedString = "string to transmitted"
-        val transmittedInt = 5
-//        val transmittedBoolean = false
-
-        intent.putExtra(MovieDetailsActivity.TRANSMITTEDSTRING,transmittedString)
-//        intent.putExtra(MovieDetailsActivity.TRANSMITTEDBOOLEAN,transmittedBoolean)
-
-        intent.putExtra(MovieDetailsActivity.TRANSMITTEDINT,transmittedInt)
-
-
         startActivity(intent)
-
-
-//        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/ru/firstSet/kotlinOne/MovieDetailsActivity.kt
+++ app/src/main/java/ru/firstSet/kotlinOne/MovieDetailsActivity.kt
@@ -7,22 +7,9 @@ import android.widget.TextView
 
 class MovieDetailsActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
-
-        val transmittedString:String? = intent.getStringExtra(TRANSMITTEDSTRING)
-        val transmittedInt: Int = intent.getIntExtra(TRANSMITTEDINT,-1)
-
-//        val textView: TextView = findViewById(R.id.movieDetailsActivityTextView1)
-//        textView.setText( transmittedString)
-//
-    }
-
-    companion object {
-        const val TRANSMITTEDSTRING = "transmittedString"
-        const val TRANSMITTEDINT = "transmittedInt"
-//        const val TRANSMITTEDBOOLEAN = true
     }
 }
+
