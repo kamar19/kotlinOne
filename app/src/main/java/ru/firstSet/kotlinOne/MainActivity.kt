74f8068500d5c412f92d055a7084c28aae6ec9a2
kamar19 <kipisov@mail.ru>
2020-11-26 16:01:17 +0600

    everything works except movie

--- app/src/main/java/ru/firstSet/kotlinOne/MainActivity.kt
+++ app/src/main/java/ru/firstSet/kotlinOne/MainActivity.kt
@@ -3,14 +3,15 @@ package ru.firstSet.kotlinOne
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.activityMainCenterTextView)
-        textView.setOnClickListener{ moveToNextScreen()}
+//        val imageView: ImageView = findViewById(R.id.fmlImageViewMovie)
+//        imageView.setOnClickListener{ moveToNextScreen()}
     }
 
     private fun moveToNextScreen() {
