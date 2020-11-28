ea60f3cefc566914fcfd09141859ac1e6d7b7e06
kamar19 <kipisov@mail.ru>
2020-11-28 15:52:21 +0600

    workshop4 the end

--- app/src/main/java/ru/firstSet/kotlinOne/MainActivity.kt
+++ app/src/main/java/ru/firstSet/kotlinOne/MainActivity.kt
@@ -1,9 +1,7 @@
 package ru.firstSet.kotlinOne
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity(), FragmentMoviesList.SomeFragmentClickListener,
@@ -25,58 +23,19 @@ class MainActivity : AppCompatActivity(), FragmentMoviesList.SomeFragmentClickLi
     }
 
     override fun onChangeFragment() {
-//        someFragment = supportFragmentManager.findFragmentByTag(FRAGMENT_TAG_MOVIES_DETAILS)
-//        if (someFragment == null) {
-//            someFragment = secondFragment
-//            someFragment.apply {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.frameLayoutContainer, secondFragment)
+            .replace(R.id.frameLayoutContainer, secondFragment, FRAGMENT_TAG_MOVIES_DETAILS)
             .commit()
-//            }
-//        } else {
-//            supportFragmentManager.beginTransaction()
-//                .show(secondFragment)
-//                .commit()
-//        }
-//        supportFragmentManager.beginTransaction()
-//            .remove(rootFragment)
-//            .commit()
+    }
+
+    override fun onChangeSecondFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayoutContainer, rootFragment, FRAGMENT_TAG_MOVIES_LIST)
+            .commit()
     }
 
     companion object {
         const val FRAGMENT_TAG_MOVIES_LIST = "MoviesList"
         const val FRAGMENT_TAG_MOVIES_DETAILS = "MoviesDetails"
     }
-
-    override fun onChangeSecondFragment() {
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.frameLayoutContainer, rootFragment)
-            .commit()
-
-//        someFragment = supportFragmentManager.findFragmentByTag(FRAGMENT_TAG_MOVIES_LIST)
-//        if (someFragment == null) {
-//            someFragment = rootFragment
-//            someFragment.apply {
-//                supportFragmentManager.beginTransaction()
-//                    .add(R.id.frameLayoutContainer, rootFragment, FRAGMENT_TAG_MOVIES_LIST)
-//                    .addToBackStack(FRAGMENT_TAG_MOVIES_LIST)
-//                    .commit()
-//            }
-//        } else {
-//            supportFragmentManager.beginTransaction()
-//                .show(rootFragment)
-//                .commit()
-//        }
-//        supportFragmentManager.beginTransaction()
-//            .remove(secondFragment)
-//            .commit()
-
-
-//        supportFragmentManager.beginTransaction()
-//            .hide(secondFragment)
-//            .show(rootFragment)
-//            .commit()
-    }
-
 }
