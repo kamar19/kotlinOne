d1f188d3ff9a53b59e6fa189df2730eb199b4e07
kamar19 <kipisov@mail.ru>
2020-11-28 15:29:34 +0600

    workshop4 the end

--- app/src/main/java/ru/firstSet/kotlinOne/FragmentMoviesDetails.kt
+++ app/src/main/java/ru/firstSet/kotlinOne/FragmentMoviesDetails.kt
@@ -7,9 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class FragmentMoviesDetails  : Fragment() {
+class FragmentMoviesDetails : Fragment() {
     private var imageViewPath2: View? = null
-    private var secondFragmentClickListener: FragmentMoviesDetails.SecondFragmentClickListener? = null
+    private var secondFragmentClickListener: FragmentMoviesDetails.SecondFragmentClickListener? =
+        null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,26 +23,26 @@ class FragmentMoviesDetails  : Fragment() {
         imageViewPath2 = view.findViewById<View>(R.id.imageViewPath2).apply {
             setOnClickListener {
                 secondFragmentClickListener?.onChangeSecondFragment()
-
             }
         }
     }
-        override fun onAttach(context: Context) {
-            super.onAttach(context)
-            if (context is SecondFragmentClickListener)
-                secondFragmentClickListener = context
-        }
 
-        override fun onDetach() {
-            super.onDetach()
-            secondFragmentClickListener = null
-        }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is SecondFragmentClickListener)
+            secondFragmentClickListener = context
+    }
 
-        fun setListener(l: SecondFragmentClickListener) {
-            secondFragmentClickListener = l
-        }
+    override fun onDetach() {
+        super.onDetach()
+        secondFragmentClickListener = null
+    }
 
-        interface SecondFragmentClickListener {
-            fun onChangeSecondFragment()
-        }
+    fun setListener(l: SecondFragmentClickListener) {
+        secondFragmentClickListener = l
+    }
+
+    interface SecondFragmentClickListener {
+        fun onChangeSecondFragment()
+    }
 }
